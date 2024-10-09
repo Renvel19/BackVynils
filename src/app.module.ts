31cc071936eb5838cb8a7a024a12ec520ef6ac3c
Renvel19 <af.renteria2@uniandes.edu.co>
2024-10-08 19:51:27 -0500

    Revert "set port dynamically"
    This reverts commit 6d3391dfa8c40236770015e9b263b868c1ce8569.

--- src/app.module.ts
+++ src/app.module.ts
@@ -37,7 +37,7 @@ import { AlbumMusicianModule } from './albummusician/albummusician.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST || 'localhost',
-      port: process.env.PORT || 5432,
+      port: 5432,
       username: process.env.DB_USER || 'postgres',
       password: process.env.DB_PASS || 'postgres',
       database: process.env.DB_NAME || 'vinyls',
